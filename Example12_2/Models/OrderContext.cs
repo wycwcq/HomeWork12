5afe5802d2c6bf7063a463604b6139c09087003d
wycwcq <1193032407@qq.com>
2020-05-26 21:42:50 +0800

    添加项目文件。

---
+++ Example12_2/Models/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebOrderService
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Goods> GoodItems { get; set; }
+    }
+}
